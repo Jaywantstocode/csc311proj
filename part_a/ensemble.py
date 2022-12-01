bd2176b779d6db9651d524b4881b9738eeb79aa5
Benson-chou <benson.chou@mail.utoronto.ca>
2022-12-01 16:25:03 -0500

    not copy and pasting

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -102,39 +102,6 @@ def bootstrapping(matrix):
     Generate a bagged dataset by randomly sampling with replacement
     """
     
-
-
-
-def evaluate(data, theta, beta):
-    # NEEDA change this into just calculating the accuracy for from bagged_predict
-    """ Evaluate the model given data and return the accuracy.
-    :param data: A dictionary {user_id: list, question_id: list,
-    is_correct: list}
-
-    :param theta: Vector
-    :param beta: Vector
-    :return: float
-    """
-    pred = []
-    for i, q in enumerate(data["question_id"]):
-        u = data["user_id"][i]
-        x = (theta[u] - beta[q]).sum()
-        p_a = sigmoid(x)
-        pred.append(p_a >= 0.5)
-    return np.sum((data["is_correct"] == np.array(pred))) \
-           / len(data["is_correct"])
-
-
-def bag_predict(data, models):
-    """
-    Get an average accuracy over each of the models.
-    """
-    preds = np.zeros(len(data['is_correct']))
-    for model in models: 
-        preds += predict(data, model[0], model[1])
-    return preds/len(models)
-    
-
 def predict(data, theta, beta):
     """ Evaluate the model given data and return prediction probabilities
     :param data: A dictionary {user_id: list, question_id: list,
@@ -151,6 +118,31 @@ def predict(data, theta, beta):
         p_a = sigmoid(x)
         pred.append(p_a)
     return pred
+
+def bag_predict(data, models):
+    """
+    Get an average accuracy over each of the models.
+    """
+    preds = np.zeros(len(data['is_correct']))
+    for model in models: 
+        preds += predict(data, model[0], model[1])
+    return preds/len(models)
+
+
+def evaluate(preds, targets):
+    # NEEDA change this into just calculating the accuracy for from bagged_predict
+    """ Evaluate the model given data and return the accuracy.
+    :param data: A dictionary {user_id: list, question_id: list,
+    is_correct: list}
+
+    :param theta: Vector
+    :param beta: Vector
+    :return: float
+    """
+    return np.sum((np.array(preds >= 0.5) == np.array(targets))) \
+           / len(preds)
+    
+
 
 
 def main():
