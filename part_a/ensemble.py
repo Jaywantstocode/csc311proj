e48fc095d8b333962f5b4b5e647e1efa3511b365
Benson-chou <benson.chou@mail.utoronto.ca>
2022-12-01 10:54:36 -0500

    add imports

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,1 +1,10 @@
-# TODO: complete this file.
+from utils import *
+import numpy as np
+from sklearn.impute import SimpleImputer 
+import matplotlib as plt
+
+
+def sigmoid(x):
+    """ Apply sigmoid function.
+    """
+    return np.exp(x) / (1 + np.exp(x))
