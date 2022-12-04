b947817c8b3033619762241953262cf4ed4332ef
Benson-chou <benson.chou@mail.utoronto.ca>
2022-12-04 18:34:18 -0500

    imma kms

--- part_a/neural_network.py
+++ part_a/neural_network.py
@@ -11,6 +11,7 @@ import torch.utils.data
 
 import numpy as np
 import torch
+import matplotlib.pyplot as plt
 
 
 def load_data(base_path="../data"):
@@ -27,6 +28,7 @@ def load_data(base_path="../data"):
         user_id: list, is_correct: list}
     """
     train_matrix = load_train_sparse(base_path).toarray()
+    train_data = load_train_csv(base_path)
     valid_data = load_valid_csv(base_path)
     test_data = load_public_test_csv(base_path)
 
@@ -37,7 +39,7 @@ def load_data(base_path="../data"):
     zero_train_matrix = torch.FloatTensor(zero_train_matrix)
     train_matrix = torch.FloatTensor(train_matrix)
 
-    return zero_train_matrix, train_matrix, valid_data, test_data
+    return zero_train_matrix, train_matrix, train_data, valid_data, test_data
 
 def sigmoid(x):
         """ Apply sigmoid function.
@@ -93,7 +95,7 @@ class AutoEncoder(nn.Module):
         return out
 
 
-def train(model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch):
+def train(model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch, train_dic):
     """ Train the neural network, where the objective also includes
     a regularizer.
 
@@ -111,6 +113,8 @@ def train(model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch):
     # Tell PyTorch you are training the model.
     model.train()
 
+    train_loss_lst = []
+    val_loss_lst = []
     # Define optimizers and loss function.
     optimizer = optim.SGD(model.parameters(), lr=lr)
     num_student = train_data.shape[0]
@@ -134,10 +138,14 @@ def train(model, lr, lamb, train_data, zero_train_data, valid_data, num_epoch):
 
             train_loss += loss.item()
             optimizer.step()
+        
+        train_loss_lst.append(eval_loss(model, zero_train_data, train_dic))
+        val_loss_lst.append(eval_loss(model, zero_train_data, valid_data))
 
         valid_acc = evaluate(model, zero_train_data, valid_data)
         print("Epoch: {} \tTraining Cost: {:.6f}\t "
               "Valid Acc: {}".format(epoch, train_loss, valid_acc))
+    return train_loss_lst, val_loss_lst
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
@@ -161,7 +169,6 @@ def evaluate(model, train_data, valid_data):
     for i, u in enumerate(valid_data["user_id"]):
         inputs = Variable(train_data[u]).unsqueeze(0)
         output = model(inputs)
-
         guess = output[0][valid_data["question_id"][i]].item() >= 0.5
         if guess == valid_data["is_correct"][i]:
             correct += 1
@@ -169,8 +176,26 @@ def evaluate(model, train_data, valid_data):
     return correct / float(total)
 
 
+def eval_loss(model, train_data, valid_data):
+    """
+    Evaluate the valid_data on the current model and get the validation loss
+    """
+    model.eval()
+
+    total = 0 
+    
+    for i, u in enumerate(valid_data["user_id"]):
+        inputs = Variable(train_data[u]).unsqueeze(0)
+        output = model(inputs)
+        target = valid_data['is_correct'][i]
+        guess = output[0][valid_data["question_id"][i]].item()
+        total += (guess - target) ** 2.
+    # print(total)
+    return total / len(valid_data['user_id'])
+
+
 def main():
-    zero_train_matrix, train_matrix, valid_data, test_data = load_data()
+    zero_train_matrix, train_matrix, train_data, valid_data, test_data = load_data()
 
     #####################################################################
     # TODO:                                                             #
@@ -199,18 +224,24 @@ def main():
         # print(f"with k: {choice}, accuracy is: {acc}")
             
 
-    optimal_k = 50
+    optimal_k = 200
     lr = 0.01
     epoch = 25
     optimal_lamb = 0.01
-
+    
     model = AutoEncoder(train_matrix.shape[1], optimal_k)
-    train(model, lr, optimal_lamb, train_matrix, zero_train_matrix, valid_data, epoch)
+    train_loss, val_loss = train(model, lr, optimal_lamb, train_matrix, zero_train_matrix, valid_data, epoch, train_data)
     val_acc = evaluate(model, zero_train_matrix, valid_data)
     test_acc = evaluate(model, zero_train_matrix, test_data)
     print(f"Validation accuracy with optimal k: {optimal_k} and optimal lambda: {optimal_lamb} is: {val_acc}")
     print(f"Test accuracy with optimal k: {optimal_k} and optimal lambda: {optimal_lamb} is: {test_acc}")
-        
+    plt.title('Training Loss and Validation Loss over Epochs')
+    plt.plot(train_loss, color = 'orange', label = 'Training Loss')
+    plt.plot(val_loss, color = 'blue', label = 'Validation Loss')
+    plt.legend(loc='best')
+    plt.ylabel('Mean Squared Error (Loss)')
+    plt.xlabel('Epochs')
+    plt.savefig("parta_q3(d).png")
     #####################################################################
     #                       END OF YOUR CODE                            #
     #####################################################################
